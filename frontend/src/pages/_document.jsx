36d4eddde3c663ca347c0e108ac2e570272e8bcd
elprofessor67 <jeeshanr599@gmail.com>
2025-11-11 00:29:08 +0530

    add meta ads

--- frontend/src/pages/_document.jsx
+++ frontend/src/pages/_document.jsx
@@ -111,6 +111,26 @@ export default function Document() {
             })(window, document, "clarity", "script", "jk5kbh82y2");
           `}
         </Script>
+
+
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s) {
+              if(f.fbq) return; n = f.fbq = function() {
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+              };
+              if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+              n.queue = []; t = b.createElement(e); t.async = !0;
+              t.src = v; s = b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t, s)
+            }(window, document, 'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1870396157186187');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </Head>
       <body style={{ backgroundColor: 'black' }}>
         <noscript
